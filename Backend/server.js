fb9cd80519175c11a10053758595da10fe1b6c03
MxVaibs <mvaibhav.main@gmail.com>
2025-08-24 11:12:18 +0530

    MongoDB setup completed.

--- Backend/server.js
+++ Backend/server.js
@@ -11,6 +11,9 @@ const port = process.env.PORT
 app.use(express.json())
 app.use(cors())
 
+const uri = process.env.URI
+mongoose.connect(uri)
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
